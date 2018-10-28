4e200be3c94cfc5e9e814a4b431a2b2415074bc1
tzyuan123 <tzyuan123@163.com>
2018-10-28 14:23:19 +0800

    远程桌面程序Demo

---
+++ RDP_Test/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace RDP_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.rdpClient = new AxMSTSCLib.AxMsRdpClient7NotSafeForScripting();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // rdpClient
+            // 
+            this.rdpClient.Enabled = true;
+            this.rdpClient.Location = new System.Drawing.Point(12, 12);
+            this.rdpClient.Name = "rdpClient";
+            this.rdpClient.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdpClient.OcxState")));
+            this.rdpClient.Size = new System.Drawing.Size(544, 429);
+            this.rdpClient.TabIndex = 1;
+            this.rdpClient.OnMouseInputModeChanged += new AxMSTSCLib.IMsTscAxEvents_OnMouseInputModeChangedEventHandler(this.rdpClient_OnMouseInputModeChanged);
+            this.rdpClient.Enter += new System.EventHandler(this.doNothing);
+            this.rdpClient.MouseCaptureChanged += new System.EventHandler(this.doNothing);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 476);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "断连";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnDisConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 580);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rdpClient);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rdpClient)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private AxMSTSCLib.AxMsRdpClient7NotSafeForScripting rdpClient;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
---
+++ RDP_Test/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MSTSCLib;
+
+namespace RDP_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+         {
+
+             rdpClient.Server = "192.168.3.162";
+             rdpClient.UserName = "tzyuan123";
+             rdpClient.AdvancedSettings2.RDPPort = 3389;
+             rdpClient.AdvancedSettings2.SmartSizing = true;
+
+             rdpClient.AdvancedSettings8.NegotiateSecurityLayer = true;
+
+             IMsTscNonScriptable secured = (IMsTscNonScriptable)rdpClient.GetOcx();
+             secured.ClearTextPassword = "tt1234";
+             rdpClient.AdvancedSettings5.ClearTextPassword = "tt1234";
+            
+             rdpClient.ColorDepth = 24;
+             rdpClient.Connect();
+         }
+
+        private void btnDisConnect_Click(object sender, EventArgs e)
+        {
+
+           rdpClient.Disconnect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doNothing(object sender, EventArgs e)
+        {
+        }
+
+        private void rdpClient_OnMouseInputModeChanged(object sender, AxMSTSCLib.IMsTscAxEvents_OnMouseInputModeChangedEvent e)
+        {
+
+        }
+    }
+
+    
+
+
+}
